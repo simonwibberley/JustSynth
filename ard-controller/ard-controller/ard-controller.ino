6659d110a8f2f71536e52ec91b765489e1d305d7
Simon Wibberley <simon.wibberley@gmail.com>
2020-02-12 10:53:32 +0000

    ffs routing done

--- ard-controller/ard-controller/ard-controller.ino
+++ ard-controller/ard-controller/ard-controller.ino
@@ -1,41 +1,136 @@
+#include <Keypad.h>
+
+const byte ROWS = 4; //four rows
+const byte COLS = 8; //three columns
+char keys[ROWS][COLS] = {
+  {'a','b','c','d','e','f','g','h'},
+  {'i','j','j','l','m','n','o','p'},
+  {'q','r','s','t','u','v','w','x'},
+  {'y','z','1','2','3','4','5','6'}
+};
+byte rowPins[ROWS] = {8, 9, 10, 11}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {7, 12, 6, 13, 5, A0, 4, A1}; //connect to the column pinouts of the keypad
+
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+
 int D1 = 6;
-int CP = 7;
-int MR = 8;
-int ITR = 2;
+int CP1 = 7;
+//int MR = 8;
+int D2 = 8;
+int CP2 = 9;
 
 int n = 0;
+int d = 0;
+
+
 
 
 void setup() {
+  Serial.begin(9600);
+
   pinMode(D1, OUTPUT);
-  pinMode(CP, OUTPUT);
-  pinMode(MR, OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(2), increment, RISING);
+  pinMode(CP1, OUTPUT);
+  
+  pinMode(D2, OUTPUT);
+  pinMode(CP2, OUTPUT);
+  
+  //pinMode(MR, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(2), incrementNumerator, RISING);
+  attachInterrupt(digitalPinToInterrupt(3), incrementDenominator, RISING);
 }
 
 
-void increment() {
+void incrementNumerator() {
   ++n;
   n = n % 16;
-  setCounter(D1, n);
+  setCounter(CP1, D1, n);
 }
 
-void setCounter(int pin, int n) {
+
+void incrementDenominator() {
+  ++d;
+  d = d % 16;
+  setCounter(CP2, D2, d);
+}
+
+void setCounter(int cpPin, int dPin, int n) {
   if(n < 8) {
     n = n | 0b10000000;
-    shiftOut(D1, CP, MSBFIRST, n);
+    shiftOut(dPin, cpPin, MSBFIRST, n);
   } else {
     n = n - 8;
     n = n << 4;
     n = n | 0b00001000;
-    shiftOut(D1, CP, MSBFIRST, n);
+    shiftOut(dPin, cpPin, MSBFIRST, n);
   }
 }
 
+void writeRatio(int ratio[]) {
+  setCounter(CP1, D1, ratio[0]-1);
+  setCounter(CP2, D2, ratio[1]-1);  
+}
+
+void writeRatio(int n, int d) {
+  setCounter(CP1, D1, n-1);
+  setCounter(CP2, D2, d-1);  
+}
+
 void loop() {
+  char key = keypad.getKey();
+
+  switch(key) {
+    case 'a':
+      writeRatio(1,1);
+      break;
+    case 'b':
+      writeRatio(16,15);
+      break;
+    case 'c':
+  writeRatio(9,8);
+      break;
+
+    case NO_KEY:
+    default:
+      break;
+    
+  }
   
-  //for(int i = 0; i < 16; ++i) {
-  //  setCounter(D1, i);
-  //  delay(1000);
-  //}
+  if (key != NO_KEY){
+    Serial.println(key);
+  }
+  
+//   delay(1);
+   //playNotes();
+}
+
+
+void playNotes() {
+  int wait = 2000;
+  writeRatio(1,1);
+  delay(wait );
+  writeRatio(16,15);
+  delay(wait );
+  writeRatio(9,8);
+  delay(wait );
+  writeRatio(6,5);
+  delay(wait );  
+  writeRatio(5,4);
+  delay(wait );  
+  writeRatio(4,3);
+  delay(wait );  
+  writeRatio(7,5);
+  delay(wait );  
+  writeRatio(3,2);
+  delay(wait );  
+  writeRatio(8,5);
+  delay(wait );  
+  writeRatio(5,3);
+  delay(wait );  
+  writeRatio(16,9);
+  delay(wait );  
+  writeRatio(15,8);
+  delay(wait );  
+  writeRatio(2,1);
+  delay(wait );  
 }
--- ffs/ffs.pro
+++ ffs/ffs.pro
@@ -1,4 +1,4 @@
-update=Tue 28 Jan 2020 23:08:10 GMT
+update=Mon 10 Feb 2020 18:51:47 GMT
 version=1
 last_client=kicad
 [general]
@@ -67,7 +67,7 @@ OthersTextUpright=1
 SolderMaskClearance=0.051
 SolderMaskMinWidth=0.25
 SolderPasteClearance=0
-SolderPasteRatio=0
+SolderPasteRatio=-0
 [pcbnew/Layer.F.Cu]
 Name=F.Cu
 Type=0
@@ -233,10 +233,21 @@ Enabled=1
 [pcbnew/Layer.F.Fab]
 Enabled=1
 [pcbnew/Layer.Rescue]
-Enabled=1
+Enabled=0
 [pcbnew/Netclasses]
 [pcbnew/Netclasses/Default]
 Name=Default
+Clearance=0.2
+TrackWidth=0.25
+ViaDiameter=0.8
+ViaDrill=0.4
+uViaDiameter=0.3
+uViaDrill=0.1
+dPairWidth=0.2
+dPairGap=0.25
+dPairViaGap=0.25
+[pcbnew/Netclasses/1]
+Name=GND
 Clearance=0.2
 TrackWidth=0.25
 ViaDiameter=0.8
